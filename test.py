76ebc199986da512e0febb1d860a26a585ee759b
Alice <alicemilea04@gmail.com>
2025-11-27 23:40:54 -0500

    training script worked!

--- test.py
+++ test.py
@@ -2,45 +2,57 @@ import torch
 import torch.nn as nn
 import timm
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
-# Load a pretrained Swin Transformer
-model = timm.create_model("swin_base_patch4_window7_224", pretrained=True)
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Check the architecture
-#print(model)
+    train_transforms = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])
+    ])
 
+    train_data = datasets.ImageFolder("sawit/data/images/train", transform=train_transforms)
+    train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=4)
 
-train_data = datasets.ImageFolder(
-    "sawit/data/images/train",
-    transform=transforms.ToTensor()
-)
+    num_classes = 7
+    model = timm.create_model("swin_base_patch4_window7_224", 
+                              pretrained=True,
+                              num_classes=num_classes, 
+                              global_pool='avg')
+    #model.head = nn.Linear(model.head.in_features, num_classes)
+    model = model.to(device)
 
-num_classes = 10
-model.head = nn.Linear(model.head.in_features, num_classes)
+    for name, param in model.named_parameters():
+        if "head" in name:
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
 
-for param in model.parameters():
-    param.requires_grad = False
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()),
+                                  lr=1e-4, weight_decay=1e-4)
 
-for param in model.head.parameters():
-    param.requires_grad = True
+    for epoch in range(5):
+        model.train()
+        running_loss = 0.0
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(
-    filter(lambda p: p.requires_grad, model.parameters()),
-    lr=1e-4,
-    weight_decay=1e-4,
-)
+            optimizer.zero_grad()
+            preds = model(images)
+            # print(preds.shape)  # should be [batch_size, num_classes]
+            # print(labels.shape) # should be [batch_size]
+            loss = criterion(preds, labels)
+            loss.backward()
+            optimizer.step()
 
+            running_loss += loss.item() * images.size(0)
 
-for epoch in range(5):
-    model.train()
-    for images, labels in train_loader:
-        images, labels = images.cuda(), labels.cuda()
-        preds = model(images)
-        loss = criterion(preds, labels)
+        epoch_loss = running_loss / len(train_loader.dataset)
+        print(f"Epoch {epoch+1} complete, loss = {epoch_loss:.4f}")
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    print(f"Epoch {epoch} complete, loss={loss.item():.4f}")
+if __name__ == "__main__":
+    main()
